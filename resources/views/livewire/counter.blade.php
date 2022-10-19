d1165d00b59ee708844140df50fdceca76ca03e8
HaudegenHH <einsundeins79@gmx.de>
2022-10-19 13:34:13 +0200

    first livewire-cmp: counter

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button wire:click="decrement" class="py-2 px-4 rounded text-2xl font-bold bg-green-400">-</button>
+    <span class="py-2 px-4 rounded text-2xl font-bold">{{ $count }}</span>
+    <button wire:click="increment" class="py-2 px-4 rounded text-2xl font-bold bg-green-400">+</button>
+</div>
